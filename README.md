472e30ea4dbbeda849c43891680455d7b906ee3a
Lauren <lhspiegel@gmail.com>
2024-04-27 15:42:35 -0700

    updates to run server

--- README.md
+++ README.md
@@ -3,8 +3,26 @@
 ## Frontend
 
 To run frontend:
-Create `frontend/.env` file that contains:
-`VITE_OPENAI_API_KEY = "YOUR KEY HERE"`
 
-Build: `npm build`
-Run: `npm run dev`
+- Create `frontend/.env` file that contains:
+  `VITE_OPENAI_API_KEY = "YOUR KEY HERE"`
+- Build: `npm build`
+- Run: `npm run dev`
+
+## Backend
+
+There are two servers. One is a flask server. One is a dockerized torchserve.
+
+For the flask server:
+
+- `conda create --name animated_drawings python=3.8.13`
+- `conda activate animated_drawings`
+- `cd anime_app/anime_app`
+- `pip install -e .`
+- In routes.py, put the absolute path to a directory to save files that the server generates. This path directory should be a sibling to the text2animation repo.
+- `export FLASK_APP=anime_app`
+- `flask run`
+
+For the docker:
+
+- Follow instructions [here](https://github.com/facebookresearch/AnimatedDrawings/tree/main)
--- anime_app/anime_app/routes.py
+++ anime_app/anime_app/routes.py
@@ -6,7 +6,7 @@ from anime_app import app
 import shutil
 
 
-PARENT_FOLDER = "/Users/juanyanli"
+PARENT_FOLDER = "/Users/LHS/Documents/pearvc-openai-hackathon/artifacts/"
 
 @app.route("/")
 def home():
@@ -18,7 +18,7 @@ def convert_to_gif():
     data = request.get_json()
     image_url = data['imageUrl']
     response = requests.get(image_url, stream=True)
-    image_path = os.path.join(PARENT_FOLDER, "Downloads/image.jpg")
+    image_path = os.path.join(PARENT_FOLDER, "image.jpg")
     if response.status_code == 200:
         with open(image_path, 'wb') as out_file:
             shutil.copyfileobj(response.raw, out_file)
@@ -26,8 +26,8 @@ def convert_to_gif():
     else:
         return "Failed to download image", 400
 
-    output = subprocess.run(["python", os.path.join(PARENT_FOLDER, "workspace/text2animation/anime_app/examples/image_to_animation.py"), image_path, os.path.join(PARENT_FOLDER, "Downloads")], capture_output=True, text=True)
+    output = subprocess.run(["python", os.path.join(PARENT_FOLDER, "../text2animation/anime_app/examples/image_to_animation.py"), image_path, os.path.join(PARENT_FOLDER)], capture_output=True, text=True)
 
     print(output)
 
-    return send_file(os.path.join(PARENT_FOLDER, 'Downloads/video.gif'), mimetype='image/gif')
+    return send_file(os.path.join(PARENT_FOLDER, 'video.gif'), mimetype='image/gif')
